0675d31c4e5ce72d4a8c727276392b4fcac4347e
Daniel Adámek <daniel.adamek123@icloud.com>
2022-12-15 21:38:44 +0100

    Vánoční úloha - import

---
+++ src/cz/tul/alg/daniela/christmas/PairDanielAdamek.java
@@ -0,0 +1,11 @@
+package cz.tul.alg.daniela.christmas;
+
+public class PairDanielAdamek<K> {
+    public K first;
+    public K second;
+
+    public PairDanielAdamek(K x, K y) {
+        first = x;
+        second = y;
+    }
+}

f320420c4e7ea25762660b48b1106e86b49b3a8c
Daniel Adámek <daniel.adamek123@icloud.com>
2022-12-15 21:10:50 +0100

    Matrix run app

---
+++ src/cz/tul/alg/daniela/matrix/MatrixApp.java
@@ -0,0 +1,42 @@
+package cz.tul.alg.daniela.matrix;
+
+public class MatrixApp {
+    public static void run() {
+        while (true) {
+            System.out.println("Rozměr matic");
+            int size = UserInterface.readSize();
+            // If size is -1, then exit the program
+            if (size == -1) return;
+
+            // Load matrix data from user
+            System.out.println("První matice");
+            Matrix m1 = UserInterface.readMatrix(size);
+
+            System.out.println("Druhá matice");
+            Matrix m2 = UserInterface.readMatrix(size);
+
+            if (m1 == null || m2 == null) {
+                System.out.println("Jedna z matic byla špatně načtena. Zadejte matice ještě jednou.\n");
+                continue;
+            }
+
+            Matrix m2Rotated = m2.deepCopy();
+
+            boolean foundRotation = false;
+            for (int i = 0; i < 4; i++) {
+                if (i != 0)
+                    m2Rotated.rotate(90);
+
+                if (Matrix.hasSameContent(m1,m2Rotated)) {
+                    System.out.println("Rotace " + i*90);
+                    foundRotation = true;
+                }
+            }
+            if (!foundRotation)
+                System.out.println("Není rotací");
+            // Run separator
+            System.out.println();
+        }
+
+    }
+}
